7f93a89e938a28ccb2b225395ff708ca9bc524cb
Ben Warren <33360056+benw1996@users.noreply.github.com>
2020-02-26 20:31:04 +0000

    Initial Commit.
    Initial commit, created project and added .gitignore file.

---
+++ HouseBuilderOOProject/HouseBuilderOOProject/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HouseBuilderOOProject {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello, here's to a new beginning...");
+
+            Console.Write("\nPress any key to end program...");
+            Console.ReadKey();
+        }
+    }
+}
